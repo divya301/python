bf644a7b0242b8560cfd0ec6b0258facdad92809
Divya.R <110725029+divya301@users.noreply.github.com>
2024-08-14 11:23:28 +0530

    Create test app.py

---
+++ test app.py
@@ -0,0 +1,11 @@
+# test_app.py
+import unittest
+from app import main
+
+class TestApp(unittest.TestCase):
+    def test_main(self):
+        # Just a placeholder test
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
